9c7eca5bfba3238986218f4c23010d4484dbcdc0
betaluis <luis.betancourt.dev@gmail.com>
2022-05-17 18:12:18 -0500

    base styles -- maybe I'll change them later to custom components, but for now I went with a base layout within the index.css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World.
-      <div className="btn text-black">
-        This is a button
-      </div>
     </div>
   );
 }
